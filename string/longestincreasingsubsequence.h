547d483b32106c5507e7ff040e9f8c247ab0379d
kanpurin <56508025+kanpurin@users.noreply.github.com>
2019-11-27 15:13:03 +0900

    Update longestincreasingsubsequence.h

--- string/longestincreasingsubsequence.h
+++ string/longestincreasingsubsequence.h
@@ -5,13 +5,14 @@
 #include <algorithm>
 #include <limits>
 
-// LIS 最長増加部分列
 template<typename T>
 T LIS(const std::vector<T> &A) {
     std::vector<T> lis(A.size(), std::numeric_limits<T>::max());
     for (int i = 0; i < (int)A.size(); i++)
     {
         lis[std::lower_bound(lis.begin(), lis.end(), A[i]) - lis.begin()] = A[i];
+        // 非減少時
+        // lis[std::upper_bound(lis.begin(), lis.end(), A[i]) - lis.begin()] = A[i]; 
     }
     return std::lower_bound(lis.begin(), lis.end(), std::numeric_limits<T>::max()) - lis.begin();
 }
