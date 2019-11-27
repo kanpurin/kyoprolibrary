a19e74ae523b6db1c2892a7d8c8b1b13134e0046
kanpurin <56508025+kanpurin@users.noreply.github.com>
2019-11-27 15:16:14 +0900

    Create longestcommonsubseqence.h

---
+++ string/longestcommonsubseqence.h
@@ -0,0 +1,31 @@
+#ifndef _LCS_H_
+#define _LCS_H_
+
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+// LCS 最長共通部分列
+template<typename T>
+int64_t LCS(const std::vector<T> &A, const std::vector<T> &B) {
+    int n = (int)A.size(), m = (int)B.size();
+    std::vector<std::vector<int64_t>> lcs(n + 1, std::vector<int64_t>(m + 1));
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= m; j++)
+        {
+            if (i == 0 || j == 0) {
+                lcs[i][j] = 0;
+            }
+            else if (A[i - 1] == B[j - 1]) {
+                lcs[i][j] = lcs[i - 1][j - 1] + 1;
+            }
+            else {
+                lcs[i][j] = std::max(lcs[i - 1][j], lcs[i][j - 1]);
+            }
+        }
+    }
+    return lcs[n][m];
+}
+
+#endif
