f6887bafe0c2d09a744c5e61c25a50e8f756885d
kanpurin <56508025+kanpurin@users.noreply.github.com>
2020-02-23 02:41:08 +0900

    Create kruskal.h

---
+++ graph/kruskal.h
@@ -0,0 +1,31 @@
+#ifndef _KRUSKAL_H_
+#define _KRUSKAL_H_
+
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+// 最小全域木 O(ElogV)?
+struct Edge {
+    long long cost, from, to;
+    bool operator>(const Edge e)const {
+        return this->cost > e.cost;
+    }
+    bool operator<(const Edge e)const {
+        return this->cost < e.cost;
+    }
+};
+
+long long Kruskal(std::vector< Edge > G, int v) {
+    sort(G.begin(),G.end());
+    UnionFind uni(v);
+    long long res = 0;
+    for(int i = 0; i < G.size(); i++) {
+        if (uni.unite(G[i].from, G[i].to)){
+            res += G[i].cost;
+        }
+    }
+    return res;
+}
+
+#endif
