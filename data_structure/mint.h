f773cce45d3e149ac91b24454b7988817045ea85
kanpurin <56508025+kanpurin@users.noreply.github.com>
2020-02-23 01:41:49 +0900

    Update mint.h

--- data_structure/mint.h
+++ data_structure/mint.h
@@ -2,50 +2,67 @@
 #define _MOD_INT_H_
 
 #include <iostream>
+#include <string>
 
-template<int mod>
+template<long long mod>
 struct mint {
 private:
-    int64_t x;
+    long long x;
 public:
-    mint(int64_t x = 0) :x(x%mod) {}
-    mint(std::string s) {
-        int64_t z = 0;
-        for(int i = 0; i < s.size(); i++) {
+    mint(long long x = 0) :x((mod+x)%mod) {}
+    mint(std::string &s) {
+        long long z = 0;
+        for (int i = 0; i < s.size(); i++) {
             z *= 10;
             z += s[i] - '0';
             z %= mod;
         }
         this->x = z;
     }
-    mint& operator+=(const mint a) {
+    mint& operator+=(const mint &a) {
         if ((x += a.x) >= mod) x -= mod;
         return *this;
     }
-    mint& operator-=(const mint a) {
+    mint& operator-=(const mint &a) {
         if ((x += mod - a.x) >= mod) x -= mod;
         return *this;
     }
-    mint& operator*=(const mint a) {
+    mint& operator*=(const mint &a) {
         (x *= a.x) %= mod;
         return *this;
     }
-    mint operator+(const mint a) const {
+    mint& operator/=(const mint &a) {
+        long long n = mod - 2;
+        mint<mod> u = 1, b = a;
+        while (n > 0) {
+            if (n & 1) {
+                u *= b;
+            }
+            b *= b;
+            n >>= 1;
+        }
+        return *this *= u;
+    }
+    mint operator+(const mint &a) const {
         mint res(*this);
         return res += a;
     }
-    mint operator-(const mint a) const {
+    mint operator-(const mint &a) const {
         mint res(*this);
         return res -= a;
     }
-    mint operator*(const mint a) const {
+    mint operator*(const mint &a) const {
         mint res(*this);
         return res *= a;
     }
-    friend std::ostream& operator<<(std::ostream& os, const mint& n) {
+    mint operator/(const mint &a) const {
+        mint res(*this);
+        return res /= a;
+    }
+    friend std::ostream& operator<<(std::ostream &os, const mint &n) {
         return os << n.x;
     }
-    bool operator==(const mint a) const {
+    bool operator==(const mint &a) const {
         return this->x == a.x;
     }
 };
