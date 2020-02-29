3df26444596bb48fb373c0268b4d4dbdfde114a7
kanpurin <56508025+kanpurin@users.noreply.github.com>
2020-02-29 19:56:21 +0900

    Update kruskal.h

--- graph/kruskal.h
+++ graph/kruskal.h
@@ -5,27 +5,44 @@
 #include <algorithm>
 #include "unionfind.h"
 
-// 最小全域木 O(ElogV)?
-struct Edge {
-    long long cost, from, to;
-    bool operator>(const Edge e)const {
-        return this->cost > e.cost;
+struct Kruskal {
+private:
+    int V; // 頂点数
+    int E; // 辺数
+    struct Edge {
+        long long cost; int from, to;
+        Edge(long long cost, int from, int to) :cost(cost), from(from), to(to) {}
+        bool operator>(const Edge e)const {
+            return this->cost > e.cost;
+        }
+        bool operator<(const Edge e)const {
+            return this->cost < e.cost;
+        }
+    };
+
+    std::vector<Edge> G;
+
+public:
+    Kruskal(int V) : V(V) {
+        E = 0;
     }
-    bool operator<(const Edge e)const {
-        return this->cost < e.cost;
+
+    void add_edge(int from, int to, long long cost) {
+        G.push_back(Edge(cost, from, to));
+        E++;
+    }
+
+    long long build() {
+        sort(G.begin(), G.end());
+        UnionFind uni(V);
+        long long res = 0;
+        for (int i = 0; i < (int)G.size(); i++) {
+            if (uni.unite(G[i].from, G[i].to)) {
+                res += G[i].cost;
+            }
+        }
+        return res;
     }
 };
-
-long long Kruskal(std::vector< Edge > G, int v) {
-    sort(G.begin(),G.end());
-    UnionFind uni(v);
-    long long res = 0;
-    for(int i = 0; i < G.size(); i++) {
-        if (uni.unite(G[i].from, G[i].to)){
-            res += G[i].cost;
-        }
-    }
-    return res;
-}
 
 #endif
