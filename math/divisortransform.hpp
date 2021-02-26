2bee4af9996b999988be28c9f7fa09e55467520b
kanpurin <kanpurin2@gmail.com>
2021-02-27 08:37:53 +0900

    上位下位の修正

--- math/divisortransform.hpp
+++ math/divisortransform.hpp
@@ -3,16 +3,16 @@
 
 #include <vector>
 
-// b[i]=Σ_{j|i}a[j] / b[i]=Σ_{i|j}a[j]
+// b[i]=Σ_{i|j}a[j] / b[i]=Σ_{j|i}a[j]
 // ゼータ変換:上位/下位の集合の和をとる, メビウス変換:逆変換
-// subset:上位集合 gcd畳み込みは下位
+// subset:下位集合 gcd畳み込みは上位
 template < class T >
 void divisor_transform(std::vector< T >& a, bool zeta, bool subset) {
     int n = a.size();
     std::vector< bool > sieve(n, false);
     for (int p = 2; p < n; ++p) {
         if (sieve[p]) continue;
-        if (!subset) {
+        if (subset) {
             if (zeta) for (int k=0; k*p<n; k++) sieve[k*p]=true, a[k*p]+=a[k];
             else for (int k=(n-1)/p; k>=0; k--) sieve[k*p]=true, a[k*p]-=a[k];
         }
