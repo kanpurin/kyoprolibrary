9de2278256e17dd545a698699849a9994b9ffda3
kanpurin <kanpurin2@gmail.com>
2021-07-17 03:18:25 +0900

    mod1000000007でのバグの修正

--- math/FormalPowerSeries.hpp
+++ math/FormalPowerSeries.hpp
@@ -88,9 +88,6 @@ private:
         else {
             int n = this->a.size(), m = b.a.size();
             static constexpr int mod0 = 998244353, mod1 = 1300234241, mod2 = 1484783617;
-            using Mint0 = mint< mod0 >;
-            using Mint1 = mint< mod1 >;
-            using Mint2 = mint< mod2 >;
             FormalPowerSeries< mod0 > l0(n), r0(m);
             FormalPowerSeries< mod1 > l1(n), r1(m);
             FormalPowerSeries< mod2 > l2(n), r2(m);
@@ -99,17 +96,27 @@ private:
             l0 *= r0;
             l1 *= r1;
             l2 *= r2;
-            this->resize(n + m - 1);
-            static const Mint1 im0 = Mint1(1) / Mint1(mod0);
-            static const Mint2 im1 = Mint2(1) / Mint2(mod1), im0m1 = im1 / mod0;
-            static const mint<MOD> m0 = mod0, m0m1 = m0 * mod1;
-            for (int i = 0; i < n + m - 1; i++) {
-                int y0 = l0.a[i].x;
-                int y1 = (im0 * (l1[i] - y0)).x;
-                int y2 = (im0m1 * (l2[i] - y0) - im1 * y1).x;
-                this->a[i] = m0m1 * y2 + y0 + m0 * y1;
-            }
+            crt(*this,l0,l1,l2);
             return *this;
+        }
+    }
+
+    template<const int MOD0, const int MOD1, const int MOD2>
+    static void crt(FPS &fps,
+             const FormalPowerSeries<MOD0> &fps0, 
+             const FormalPowerSeries<MOD1> &fps1, 
+             const FormalPowerSeries<MOD2> &fps2) {
+        assert(fps0.size() == fps1.size() && fps0.size() == fps2.size());
+        int n = (int)fps0.size();
+        fps.resize(n);
+        static const mint<MOD1> im0 = mint<MOD1>(MOD0).inv();
+        static const mint<MOD2> im1 = mint<MOD2>(MOD1).inv(), im0m1 = im1/MOD0;
+        static const mint<MOD> m0 = MOD0, m0m1 = m0*MOD1;
+        for (int i = 0; i < n; i++) {
+            int y0 = fps0.a[i].x;
+            int y1 = (im0*(fps1.a[i]-y0)).x;
+            int y2 = (im0m1*(fps2.a[i]-y0)-im1*y1).x;
+            fps.a[i] = m0m1*y2+y0+m0*y1;
         }
     }
 
@@ -136,17 +143,28 @@ public:
         this->a.resize(sz,m);
     }
 
-    FPS operator-(const FPS &a) const { return FPS(*this) -= a; }
+    FPS operator+(const long long a) const { return FPS(*this) += a; }
     FPS operator+(const FPS &a) const { return FPS(*this) += a; }
+    FPS operator-(const long long a) const { return FPS(*this) -= a; }
+    FPS operator-(const FPS &a) const { return FPS(*this) -= a; }
     FPS operator*(const long long a) const { return FPS(*this) *= a; }
     FPS operator*(const FPS &a) const { return FPS(*this) *= a; }
+    FPS operator/(const FPS &a) const { return FPS(*this) /= a; }
+    FPS &operator+=(const long long v) {
+        this->a[0] += v;
+        return *this;
+    }
     FPS &operator+=(const FPS &r) {
-        this->resize(std::max((int)this->size(),r.size()));
+        this->resize(max((int)this->size(),r.size()));
         for(int i = 0; i < (int)r.size(); i++) this->a[i] += r.a[i];
         return *this;
     }
+    FPS &operator-=(const long long v) {
+        this->a[0] -= v;
+        return *this;
+    }
     FPS &operator-=(const FPS &r) {
-        this->resize(std::max((int)this->size(),r.size()));
+        this->resize(max((int)this->size(),r.size()));
         for(int i = 0; i < (int)r.size(); i++) this->a[i] -= r.a[i];
         return *this;
     }
@@ -156,6 +174,10 @@ public:
     }
     FPS &operator*=(const FPS &r) {
         this->convolution_inplace(r);
+        return *this;
+    }
+    FPS &operator/=(const FPS &r) {
+        this->convolution_inplace(r.inverse());
         return *this;
     }
 
@@ -168,23 +190,70 @@ public:
         int n = this->size();
         assert(n != 0 && this->a[0].x != 0);
         if(deg == -1) deg = n;
-        FPS r({this->a[0].inv()});
-        for(int m=1;m<deg;m*=2) {
-            FPS f(std::vector<mint<MOD>>(this->a.begin(), this->a.begin() + std::min(n, 2*m)));
-            FPS g(r);
-            f.resize(2*m), f.ntt(false);
-            g.resize(2*m), g.ntt(false);
-            for (int i = 0; i < 2*m; i++) f[i] *= g[i];
-            f.ntt(true);
-            f.a.erase(f.a.begin(), f.a.begin() + m);
-            f.resize(2*m), f.ntt(false);
-            for (int i = 0; i < 2*m; i++) f[i] *= g[i];
-            f.ntt(true); 
-            for (int i = 0; i < 2*m; i++) f[i] = -f[i];
-            r.a.insert(r.a.end(), f.a.begin(), f.a.begin() + m);
+
+        if (!any) {
+            FPS r({this->a[0].inv()});
+            for(int m=1;m<deg;m*=2) {
+                FPS f(std::vector<mint<MOD>>(this->a.begin(), this->a.begin() + std::min(n, 2*m)));
+                FPS g(r);
+                f.resize(2*m), f.ntt(false);
+                g.resize(2*m), g.ntt(false);
+                for (int i = 0; i < 2*m; i++) f[i] *= g[i];
+                f.ntt(true);
+                f.a.erase(f.a.begin(), f.a.begin() + m);
+                f.resize(2*m), f.ntt(false);
+                for (int i = 0; i < 2*m; i++) f[i] *= g[i];
+                f.ntt(true); 
+                for (int i = 0; i < 2*m; i++) f[i] = -f[i];
+                r.a.insert(r.a.end(), f.a.begin(), f.a.begin() + m);
+            }
+            return r.low(deg);
         }
-        return r.low(deg);
+        else {
+            // int n = this->a.size();
+            // static constexpr int mod0 = 998244353, mod1 = 1300234241, mod2 = 1484783617;
+            // FPS ret;
+            // f0 = f0.inverse(deg);
+            // f1 = f1.inverse(deg);
+            // f2 = f2.inverse(deg);
+            // crt(ret,f0,f1,f2);
+            // return ret;
+
+            FPS r({this->a[0].inv()});
+            for (int i = 1; i < deg; i <<= 1)
+                r = (r*2 - r.square()*(*this).low(i<<1)).low(i<<1);
+            return r.low(deg);
+        }
     }
+
+    // f(x)^2
+    FPS& square_inplace() {
+        if (this->size() == 0) {
+            return *this;
+        }
+        if (!any) {
+            int n = this->size(), sz = 1 << std::__lg(2*(n+n-1)-1);
+            if (n <= 60) return *this = convolution_naive(*this,*this);
+            this->resize(sz), this->ntt(false);
+            for (int i = 0; i < sz; i++) this->a[i] *= this->a[i];
+            this->ntt(true), this->resize(n+n-1);
+            return *this;
+        }
+        else {
+            int n = this->a.size();
+            static constexpr int mod0 = 998244353, mod1 = 1300234241, mod2 = 1484783617;
+            FormalPowerSeries< mod0 > f0(n);
+            FormalPowerSeries< mod1 > f1(n);
+            FormalPowerSeries< mod2 > f2(n);
+            for (int i = 0; i < n; i++) f0.a[i] = this->a[i].x, f1.a[i] = this->a[i].x, f2.a[i] = this->a[i].x;
+            f0.square_inplace();
+            f1.square_inplace();
+            f2.square_inplace();
+            crt(*this,f0,f1,f2);
+            return *this;
+        }
+    }
+    FPS square() const { return FPS(*this).square_inplace(); }
 
     // f'(x)
     FPS& differential_inplace() {
@@ -225,62 +294,73 @@ public:
 
     // exp(f(x)) ’è”€‚Í0‚Å‚ ‚é
     FPS& exp_inplace(int deg = -1) {
-        int n = this->size();
-        assert(n > 0 && (*this)[0] == 0);
-        if (deg == -1) deg = n;
-        assert(deg >= 0);
-        FPS g({1}), g_fft;
-        this->resize(deg);
-        this->a[0] = 1;
-        FPS h_drv = this->differential();
-        for (int m = 1; m < deg; m *= 2) {
-            FPS f_fft(std::vector<mint<MOD>>(this->a.begin(), this->a.begin() + m));
-            f_fft.resize(2*m), f_fft.ntt(false);
-            mint<MOD> invm = m; invm = invm.inv();
+        if (!any) {
+            int n = this->size();
+            assert(n > 0 && (*this)[0] == 0);
+            if (deg == -1) deg = n;
+            assert(deg >= 0);
+            FPS g({1}), g_fft;
+            this->resize(deg);
+            this->a[0] = 1;
+            FPS h_drv = this->differential();
+            for (int m = 1; m < deg; m *= 2) {
+                FPS f_fft(std::vector<mint<MOD>>(this->a.begin(), this->a.begin() + m));
+                f_fft.resize(2*m), f_fft.ntt(false);
+                mint<MOD> invm = m; invm = invm.inv();
 
-            if (m > 1) {
-                FPS _f(m);
-                for(int i = 0; i < m; i++) _f[i] = f_fft[i] * g_fft[i];
-                _f.ntt(true);
-                _f.a.erase(_f.a.begin(), _f.a.begin() + m/2);
-                _f.resize(m), _f.ntt(false);
-                for(int i = 0; i < m; i++) _f[i] *= g_fft[i];
-                _f.ntt(true);
-                _f.resize(m/2);
-                for (int i = 0; i < m/2; i++) _f[i] = -_f[i];
-                g.a.insert(g.a.end(), _f.a.begin(), _f.a.begin() + m/2);
+                if (m > 1) {
+                    FPS _f(m);
+                    for(int i = 0; i < m; i++) _f[i] = f_fft[i] * g_fft[i];
+                    _f.ntt(true);
+                    _f.a.erase(_f.a.begin(), _f.a.begin() + m/2);
+                    _f.resize(m), _f.ntt(false);
+                    for(int i = 0; i < m; i++) _f[i] *= g_fft[i];
+                    _f.ntt(true);
+                    _f.resize(m/2);
+                    for (int i = 0; i < m/2; i++) _f[i] = -_f[i];
+                    g.a.insert(g.a.end(), _f.a.begin(), _f.a.begin() + m/2);
+                }
+
+                FPS t(std::vector<mint<MOD>>(this->a.begin(), this->a.begin() + m)); 
+                t.differential_inplace();
+                {
+                    FPS r(std::vector<mint<MOD>>(h_drv.a.begin(), h_drv.a.begin() + m-1));
+                    r.resize(m); r.ntt(false);
+                    for (int i = 0; i < m; i++) r.a[i] *= f_fft.a[i];
+                    r.ntt(true);
+                    t -= r;
+                    t.a.insert(t.a.begin(), t.a.back()); t.a.pop_back();
+                }
+
+                t.resize(2*m); t.ntt(false); 
+                g_fft = g; g_fft.resize(2*m); g_fft.ntt(false);
+                for (int i = 0; i < 2*m; i++) t.a[i] *= g_fft.a[i];
+                t.ntt(true);
+                t.resize(m);
+                
+                FPS v(std::vector<mint<MOD>>(this->a.begin() + m, this->a.begin() + std::min(deg, 2*m))); v.resize(m);
+                t.a.insert(t.a.begin(), m-1, 0); t.a.push_back(0);
+                t.integral_inplace();
+                for (int i = 0; i < m; i++) v.a[i] -= t.a[m+i];
+
+                v.resize(2*m); v.ntt(false);
+                for (int i = 0; i < 2*m; i++) v.a[i] *= f_fft.a[i];
+                v.ntt(true);
+                v.resize(m);
+
+                for (int i = 0; i < std::min(deg-m,m); i++) this->a[m+i] = v.a[i];
             }
-
-            FPS t(std::vector<mint<MOD>>(this->a.begin(), this->a.begin() + m)); 
-            t.differential_inplace();
-            {
-                FPS r(std::vector<mint<MOD>>(h_drv.a.begin(), h_drv.a.begin() + m-1));
-                r.resize(m); r.ntt(false);
-                for (int i = 0; i < m; i++) r.a[i] *= f_fft.a[i];
-                r.ntt(true);
-                t -= r;
-                t.a.insert(t.a.begin(), t.a.back()); t.a.pop_back();
-            }
-
-            t.resize(2*m); t.ntt(false); 
-            g_fft = g; g_fft.resize(2*m); g_fft.ntt(false);
-            for (int i = 0; i < 2*m; i++) t.a[i] *= g_fft.a[i];
-            t.ntt(true);
-            t.resize(m);
-            
-            FPS v(std::vector<mint<MOD>>(this->a.begin() + m, this->a.begin() + std::min(deg, 2*m))); v.resize(m);
-            t.a.insert(t.a.begin(), m-1, 0); t.a.push_back(0);
-            t.integral_inplace();
-            for (int i = 0; i < m; i++) v.a[i] -= t.a[m+i];
-
-            v.resize(2*m); v.ntt(false);
-            for (int i = 0; i < 2*m; i++) v.a[i] *= f_fft.a[i];
-            v.ntt(true);
-            v.resize(m);
-
-            for (int i = 0; i < std::min(deg-m,m); i++) this->a[m+i] = v.a[i];
+            return *this;
         }
-        return *this;
+        else {
+            assert(this->size() == 0 || this->a[0] == 0);
+            if (deg == -1) deg = (int)this->size();
+            FPS r({1});
+            for (int i = 1; i < deg; i <<= 1) {
+                r = (r*(this->low(i << 1)+1-r.log(i << 1))).low(i << 1);
+            }
+            return *this = r.low(deg);
+        }
     }
     FPS exp(const int deg = -1) const { return FPS(*this).exp_inplace(deg); }
 
