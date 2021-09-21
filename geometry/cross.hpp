3f3650e3fe580b5406d69943dc901f8b81b4ea7e
kanpurin <kanpurin2@gmail.com>
2021-09-21 13:12:23 +0900

    [fix]inline関数に変更

--- geometry/cross.hpp
+++ geometry/cross.hpp
@@ -4,17 +4,17 @@
 #include "template.hpp"
 
 // AB×CD
-coord_t cross(const Point &A, const Point &B, const Point &C, const Point &D) {
+inline coord_t cross(const Point &A, const Point &B, const Point &C, const Point &D) {
     return (B.x - A.x) * (D.y - C.y) - (B.y - A.y) * (D.x - C.x);
 }
 
 // AB×AC
-coord_t cross(const Point &A, const Point &B, const Point &C) {
+inline coord_t cross(const Point &A, const Point &B, const Point &C) {
     return cross(A,B,A,C);
 }
 
 // OA×OB
-coord_t cross(const Point &A, const Point &B) {
+inline coord_t cross(const Point &A, const Point &B) {
     return cross(Point(0,0),A,B);
 }
 
