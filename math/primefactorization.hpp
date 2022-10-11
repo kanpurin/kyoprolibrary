b681e2e324a21053ea64eebd8fdac492bb333994
kanpurin <kanpurin2@gmail.com>
2022-10-11 22:51:01 +0900

    [create]ポラードロー法を用いた素因数分解の作成

--- math/primefactorization.hpp
+++ math/primefactorization.hpp
@@ -2,21 +2,67 @@
 #define _PRIME_FACTORIZATION_HPP_
 
 #include <vector>
+#include <algorithm>
 
-// 素因数分解 O(√n)以下
+#include "isprime.hpp"
+
+// 素因数分解 O(N^(1/4))
 // (素因数,個数)
-template<typename T>
-std::vector<std::pair<T,int>> prime_factorization(T n) {
-    std::vector<std::pair<T, int>> res;
-    for (T i = 2; i*i <= n; i++) {
+long long PollardRho(long long N) {
+    if (N % 2 == 0) return 2;
+    long long m = 1LL<<(70-__builtin_clrsbll(N))/8;
+    for (long long c = 1; c < N; c++) {
+        auto f = [&](long long x) { return ((__int128_t)x*x+c)%N; };
+        long long x,y,g,q,r,k,ys;
+        y = k = 0;
+        g = q = r = 1;
+        while (g == 1) {
+            x = y;
+            while(k < 3*r/4) {
+                y = f(y);
+                k += 1;
+            }
+            while(k < r && g == 1) {
+                ys = y;
+                for (int _ = 0; _ < std::min(m,r-k); _++) {
+                    y = f(y);
+                    q = (__int128_t)q*abs(x-y)%N;
+                }
+                g = std::__gcd(q,N);
+                k += m;
+            }
+            k = r;
+            r <<= 1;
+        }
+        if (g == N) {
+            g = 1;
+            y = ys;
+            while(g == 1) {
+                y = f(y);
+                g = std::__gcd((long long)abs(x-y),N);
+            }
+        }
+        if (g == N) continue;
+        if (isprime(g)) return g;
+        else if (isprime(N/g)) return N/g;
+        else return PollardRho(g);
+    }
+    return -1; // error
+}
+
+std::vector<std::pair<long long,int>> prime_factorization(long long N) {
+    std::vector<std::pair<long long,int>> res;
+    while(!isprime(N) && N > 1) {
+        long long p = PollardRho(N);
         int cnt = 0;
-        while (n % i == 0) {
-            n /= i;
+        while(N%p==0) {
+            N /= p;
             cnt++;
         }
-        if (cnt > 0) res.push_back({i,cnt});
+        res.push_back({p,cnt});
     }
-    if (n > 1) res.push_back({n,1});
+    if (N > 1) res.push_back({N,1});
+    sort(res.begin(), res.end());
     return res;
 }
 
