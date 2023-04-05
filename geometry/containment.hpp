9bcd0f76d7580201b51de6588a56bbea24c2decb
kanpurin <kanpurin2@gmail.com>
2023-04-05 21:34:01 +0900

    [update]高速なアルゴリズムの実装

--- geometry/containment.hpp
+++ geometry/containment.hpp
@@ -6,19 +6,32 @@
 #include "template.hpp"
 #include "dot.hpp"
 #include "cross.hpp"
+#include "ccw.hpp"
 
+// O(logN)
 // 0:含まれない 1:辺上にある 2:含まれる
+// 反時計回りに与えられる
 int containment(const std::vector<Point> &polygon, const Point &point) {
     int n = polygon.size();
-    bool in = false;
-    for(int i = 0; i < n; i++) {
-        Point a(polygon[i].x-point.x,polygon[i].y-point.y);
-        Point b(polygon[(i+1)%n].x-point.x,polygon[(i+1)%n].y-point.y);
-        if (a.y > b.y) std::swap(a,b);
-        if (a.y <= 0 && 0 < b.y && cross(a,b) < 0) in = !in;
-        if (cross(a,b) == 0 && dot(a,b) <= 0) return 1;
+    int l = 1, r = n-1;
+    Point initpoint = polygon[0];
+    if (cross(initpoint, polygon[l], point) < 0 ||
+        cross(initpoint, point, polygon[r]) < 0) {
+        return 0;
     }
-    return in ? 2 : 0;
+    if (ccw(initpoint,polygon[l],point) == 0 || 
+        ccw(initpoint,polygon[r],point) == 0) {
+        return 1;
+    }
+    while(r-l>1) {
+        int m = (l+r)/2;
+        if (cross(initpoint, polygon[l], point) >= 0 &&
+            cross(initpoint, point, polygon[m]) >= 0)
+            r = m;
+        else
+            l = m;
+    }
+    return ccw(polygon[l],polygon[r],point) + 1;
 }
 
 #endif
